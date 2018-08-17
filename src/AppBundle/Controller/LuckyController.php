d8ec5e846d06fb99bf54024680093ff36a00482f
Bruno Maurício Willrich Gehlen <b.gehlen@tjrs.jus.br>
2018-08-17 09:39:44 -0300

    testing routes for the first time

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,29 @@
+<?php
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+    /**
+     * @Route("teste/test-action")
+     */
+    public function testFunctionAction() 
+    {
+        return new Response('It works!');
+    }
+}
